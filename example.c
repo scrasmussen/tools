5dd74ff07f6ec39342c43d2763db359249d96b39
Soren Rasmussen <s.c.rasmussen@gmail.com>
2018-09-24 10:46:48 +0100

    Cleaned code

--- example.c
+++ example.c
@@ -4,11 +4,11 @@
 
 int main()
 {
-  int i, num, j;
+  int i, j, num;
   printf ("Enter the number: ");
   scanf ("%d", &num );
 
-  for (i=1; i<num; i++)
+  for (i=1; i<=num; i++)
     j=j*i;
 
   printf("The factorial of %d is %d\n",num,j);
